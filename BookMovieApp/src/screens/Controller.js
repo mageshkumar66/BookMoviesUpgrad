9ca1014cd5d82bb9add591cb9f653e27ffe08b77
yashnigamp <yashnigam.p@gmail.com>
2023-02-26 16:44:48 +0530

    Book Show

--- BookMovieApp/src/screens/Controller.js
+++ BookMovieApp/src/screens/Controller.js
@@ -4,7 +4,7 @@ import Details from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
-import Header from "../common/header/Header";
+
 
 const Controller = () => {
   const baseUrl = "/api/v1/";
@@ -17,7 +17,7 @@ const Controller = () => {
           path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         />
-        {/* <Route
+        <Route
           path="/movie/:id"
           render={(props) => <Details {...props} baseUrl={baseUrl} />}
         />
@@ -28,7 +28,7 @@ const Controller = () => {
         <Route
           path="/confirm/:id"
           render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-        /> */}
+        />
       </div>
     </Router>
   );
